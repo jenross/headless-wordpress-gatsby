09fa200517c5301f530931104df3b7f84fa72d62
jenross <ross.m.jennifer@gmail.com>
2020-05-23 14:42:48 -0400

    querying sample page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default function Home() {
-  return <h1>Hello Gatsby!</h1>
+  const data = useStaticQuery(graphql`
+    {
+      allWordpressPage {
+        edges {
+          node {
+            title
+            content
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div>
+      {data.allWordpressPage.edges.map(page => (
+        <>
+          <div key={page.node.id}>
+            <h1>{page.node.title}</h1>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: page.node.content }} />
+        </>
+      ))}
+    </div>
+  )
 }
