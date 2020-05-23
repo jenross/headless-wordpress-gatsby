3bd2fe50f87b0abaeac5d0af8870bb035c582c53
jenross <ross.m.jennifer@gmail.com>
2020-05-23 15:02:28 -0400

    templates for pages and posts; node config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,3 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-
-export default function Home() {
-  const data = useStaticQuery(graphql`
-    {
-      allWordpressPage {
-        edges {
-          node {
-            title
-            content
-          }
-        }
-      }
-    }
-  `)
-
-  return (
-    <div>
-      {data.allWordpressPage.edges.map(page => (
-        <>
-          <div key={page.node.id}>
-            <h1>{page.node.title}</h1>
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: page.node.content }} />
-        </>
-      ))}
-    </div>
-  )
+export default function () {
+  return null
 }
